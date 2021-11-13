38b9367e1c42017b7ed576239407e464a7afcd26
AranBorkum <aranborkum@hotmail.com>
2021-11-13 05:50:16 -0600

    Full Geometry job files and low background job files

---
+++ LowBackground/RunJobs.sh
@@ -0,0 +1,4 @@
+project.py --stage $1 --$2 --xml RadiologicalNeutrons_200cmEarly_LowBck_larfd.xml
+project.py --stage $1 --$2 --xml RadiologicalNeutrons_200cm_LowBck_larfd.xml
+project.py --stage $1 --$2 --xml RadiologicalNeutrons_200cmTh_LowBck_larfd.xml
+
